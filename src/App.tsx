0ac1ec9559b11d2bd7f55bc58397ae3b89478718
dvt-takahisa-sakurai <takahisa.sakurai@bcgdv.com>
2022-08-02 07:17:21 +0900

    Boilerplate UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { registerRootComponent } from "expo";
 import { Amplify } from "aws-amplify";
 import awsconfig from "./aws-exports";
-Amplify.configure(awsconfig);
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import Home from "./Home";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar />
+      <Home />
+    </View>
+  );
+}
+
+Amplify.configure(awsconfig);
+registerRootComponent(App);
